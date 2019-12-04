f253b0b2c5b1711b92b7b64a9cd0ef5f1bf58e6a
Sairam Kandakatla <sairam_kandakatla@gap.com>
2019-12-04 06:37:45 +0530

    first commit

---
+++ src/main/java/com/saiit/coredemo/CoredemoApplication.java
@@ -0,0 +1,13 @@
+package com.saiit.coredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoredemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saiit/coredemo/CoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saiit.coredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
